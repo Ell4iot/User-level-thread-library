f4e2b0c06f5930128bfed5125777955b2b715c1f
yuhwang421 <59779593+yuhwang421@users.noreply.github.com>
2022-02-06 11:13:38 -0800

    none

--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -9,12 +9,35 @@
 
 #include "private.h"
 #include "uthread.h"
-
+#include "queue.h"
 /* TODO */
+
+queue_t ready_queue = queue_create();
+
+struct TCB {
+    uthread_t TID;
+    uthread_ctx_t context;
+    // 0 for running, 1 for ready, 2 for blocked, 3 for zombie
+    int state;
+    void* stack;
+    uthread_func_t* func;
+};
+
+typedef struct TCB* TCB_t;
 
 int uthread_start(int preempt)
 {
 	/* TODO */
+    void(preempt);
+
+    // create the main thread
+    TCB_t thread = (TCB_t) malloc(sizeof(struct TCB));
+    thread->TID = 0;
+    queue_enqueue(ready_queue, thread);
+
+
+
+
 	return -1;
 }
 
@@ -27,6 +50,14 @@ int uthread_stop(void)
 int uthread_create(uthread_func_t func)
 {
 	/* TODO */
+    // create new stack object
+    TCB_t thread = (TCB_t) malloc(sizeof(struct TCB));
+    thread->TID = 0;
+    queue_enqueue(ready_queue, thread);
+
+    // allocate stack segment
+    uthread_ctx_alloc_stack()
+    uthread_ctx_init()
 	return -1;
 }
 
