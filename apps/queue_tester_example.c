5d34348af5ed3468d0741ba527e6082dcaf72b59
joseph6s <75140405+joseph6s@users.noreply.github.com>
2022-02-10 17:50:53 -0800

    update tester case

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -15,71 +15,32 @@ do {									\
 	}									\
 } while(0)
 
-static int inc_item(queue_t q, void *data, void *arg)
+/* Create */
+void test_create(void)
 {
-    int *a = (int*)data;
-    int inc = (int)(long)arg;
+	fprintf(stderr, "*** TEST create ***\n");
 
-    if (*a == 42)
-        queue_delete(q, data);
-    else
-        *a += inc;
-
-    return 0;
+	TEST_ASSERT(queue_create() != NULL);
 }
 
-/* Callback function that finds a certain item according to its value */
-static int find_item(queue_t q, void *data, void *arg)
+/* Enqueue/Dequeue simple */
+void test_queue_simple(void)
 {
-    int *a = (int*)data;
-    int match = (int)(long)arg;
-    (void)q; //unused
+	int data = 3, *ptr;
+	queue_t q;
 
-    if (*a == match)
-        return 1;
+	fprintf(stderr, "*** TEST queue_simple ***\n");
 
-    return 0;
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_dequeue(q, (void**)&ptr);
+	TEST_ASSERT(ptr == &data);
 }
-static int printint(queue_t q, void *data, void *arg)
-{
-    (void)q;
-    (void)arg;
-
-    int *a = (int*) data;
-    printf("%d\n", *a);
-    return 0;
-}
-
-void test_iterator(void)
-{
-    queue_t q;
-    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
-    size_t i;
-    int *ptr;
-
-    /* Initialize the queue and enqueue items */
-    q = queue_create();
-    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
-        queue_enqueue(q, &data[i]);
-
-    /* Add value '1' to every item of the queue, delete item '42' */
-    queue_iterate(q, inc_item, (void*)1, NULL);
-    queue_iterate(q, printint, NULL, NULL);
-    assert(data[0] == 2);
-    assert(queue_length(q) == 9);
-
-    /* Find and get the item which is equal to value '5' */
-    ptr = NULL;     // result pointer *must* be reset first
-    queue_iterate(q, find_item, (void*)5, (void**)&ptr);
-    assert(ptr != NULL);
-    assert(*ptr == 5);
-    assert(ptr == &data[3]);
-}
-
 
 int main(void)
 {
-    test_iterator();
+	test_create();
+	test_queue_simple();
 
-    return 0;
+	return 0;
 }
